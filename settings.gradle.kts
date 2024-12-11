5054cea2a09acf16859209f5a4d02558b0b82d67
Kenny Cason <kenneth.cason@gmail.com>
2024-12-11 11:51:34 -0800

    move game examples into game directory

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,11 @@ rootProject.name = "kengine"
 
 val isPlaydateEnabled = System.getenv("PLAYDATE_SDK_PATH") != null
 println("Playdate enabled: $isPlaydateEnabled")
-val modules = mutableListOf("kengine", "boxxle", "helloworld", "image-shuffle")
+
+val modules = mutableListOf("kengine")
 if (isPlaydateEnabled) {
     modules.add("kengine-playdate")
 }
+modules.addAll(listOf("games:boxxle", "games:helloworld", "games:image-shuffle"))
 
 include(modules)
